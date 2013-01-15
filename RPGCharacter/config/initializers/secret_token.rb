c0da69ed184dd8ed1ea7b180b245217407f6b088
David Savrda <korahel@gmail.com>
2013-01-15 08:15:56 -0500

    Take Two

---
+++ RPGCharacter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPGCharacter::Application.config.secret_token = 'e1bc145cfc12be25ddecbcbcc6016a8b4c530510d1c9ac79ed0e12ec3ba19e90d404549ba462e7216891a9f20eaea4e7eb2b521376cbd691711a6d956077ea32'
