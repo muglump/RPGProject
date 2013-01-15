619e3902a97e0e9649d2e7408307e7f476f5e3ba
Andrew Piliser <pilisera@gmail.com>
2013-01-15 09:31:29 -0500

    Updated User model and migrate files

--- RPGCharacter/db/migrate/20130115135321_create_users.rb
+++ RPGCharacter/db/migrate/20130115135321_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.Integer :UserID
-      t.String :Name
-      t.String :Hash
+      t.Integer :userID
+      t.String :name
+      t.String :password
 
       t.timestamps
     end
