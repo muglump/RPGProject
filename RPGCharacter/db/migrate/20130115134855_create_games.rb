2b5185883cf1bc746f6da707a7178d7ef1ba77f1
David Savrda <korahel@gmail.com>
2013-01-15 09:48:53 -0500

    Game migration

--- RPGCharacter/db/migrate/20130115134855_create_games.rb
+++ RPGCharacter/db/migrate/20130115134855_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
-    create_table :games, {:primary_key => :GameID} do |t|
-      t.Integer :GameID, :null => false, :default => ""
-      t.Integer :SystemID, :null => false, :default =>""
+    create_table :games, {:primary_key => :gameID} do |t|
+      t.Integer :gameID, :null => false, :default => ""
+      t.foreign_key :systemID
 
       t.timestamps
     end
