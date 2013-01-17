09f737b16da3a3866caae0edc2b441dfe7992c26
Andrew Piliser <pilisera@gmail.com>
2013-01-16 22:59:20 -0500

    starting the db

--- RPGProject/RPGChar.rb
+++ RPGProject/RPGChar.rb
@@ -1,8 +1,9 @@
 require 'sqlite3'
 
 begin
-  db = SQLite3::Database.new ":memory:"
-  puts db.get_first_value 'SELECT SQLITE_VERSION()'
+  db = SQLite3::Database.open "RPGChar.db"
+  db.execute "CREATE TABLE IF NOT EXISTS User(userID INTEGER PRIMARY KEY, name TEXT, password TEXT)"
+  db.execute "INSERT INTO User VALUES(1, 'Bob', 'asdf')"
 
 rescue SQLite3::Exception => e
   puts "Exception occured"
