c49a6c956ab8d0f3bd9b9cc33c275b1062ddf776
David Savrda <korahel@gmail.com>
2013-01-16 22:12:43 -0500

    Allright

---
+++ RPGProject/RPGProject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPGProject::Application.config.secret_token = 'f8bc84fd613400d24df75746f405ec4ad1449a4c1caff38478576c9dd70eba3396990b37ea11a8b15e8b0f2188c3ff17fe9b28977e75f0b853e3feba199e87d1'
