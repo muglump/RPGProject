af3c2d14379d8a67829aaa4bc86b6bbd0eeb5cea
Andrew Piliser <pilisera@gmail.com>
2013-01-16 22:53:52 -0500

    working things out

--- RPGProject/RPGChar.rb
+++ RPGProject/RPGChar.rb
@@ -1,6 +1,13 @@
-require 'sinatra'
-require 'haml'
+require 'sqlite3'
 
-get '/' do
-  "Hello world!"
+begin
+  db = SQLite3::Database.new ":memory:"
+  puts db.get_first_value 'SELECT SQLITE_VERSION()'
+
+rescue SQLite3::Exception => e
+  puts "Exception occured"
+  puts e
+  
+ensure 
+  db.close if db
 end
