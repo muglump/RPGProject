d887788647b53470e0ef3ae45f62d29ffbbef09c
David Savrda <korahel@gmail.com>
2013-01-24 09:09:13 -0500

    Login Proc

--- RPGProject/SPROC.sql
+++ RPGProject/SPROC.sql
@@ -22,3 +22,16 @@ BEGIN
  	-- Return success
  	RETURN 0;
 END //
+
+DROP FUNCTION RPGChar.LogInUser
+DELIMITER //
+-- Returns 0 on successful login
+--Returns 1 on invalid login information
+CREATE FUNCTION RPGChar.LogInUser(
+		_Username text, 
+		_Password text)
+RETURNS int
+BEGIN 
+ IF (Select Password from User Where name = _ Username) = _Password THEN
+ 	RETUrn 0;
+ return 1;
